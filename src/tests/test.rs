4c8ed49f32f68f387a8922025ccae6c65b5f9fbc
Kaustab Bharasa <accessphysics537@gmail.com>
2025-07-26 00:32:57 +0530

    updated tests

--- src/tests/test.rs
+++ src/tests/test.rs
@@ -8,11 +8,11 @@ fn test_sma_calculations() {
     };
 
     let prices = vec![5.0, 7.0, 13.0, 16.0, 18.0, 22.0, 25.0, 29.0];
-    let ema = <TechnicalIndicators as StrategyCalculations>::calculate_ema(&prices, 5);
-    println!("EMA: {:?}", ema);
-    assert_eq!(ema.len(), 4);
-    assert_eq!(ema[0], 11.8);
-    assert_eq!(ema[1], 15.2);
-    assert_eq!(ema[2], 18.8);
-    assert_eq!(ema[3], 22.0);
+    let sma = <TechnicalIndicators as StrategyCalculations>::calculate_sma(&prices, 5);
+    println!("EMA: {:?}", sma);
+    assert_eq!(sma.len(), 4);
+    assert_eq!(sma[0], 11.8);
+    assert_eq!(sma[1], 15.2);
+    assert_eq!(sma[2], 18.8);
+    assert_eq!(sma[3], 22.0);
 }
